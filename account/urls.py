dbc292492e157fced3b8355954f84dff24f630f9
Supra_1904 <koshtisahil02@gmail.com>
2025-09-22 21:15:03 +0530

    -Add the register.html -Add the RoleBasedLoginRedirect

--- account/urls.py
+++ account/urls.py
@@ -3,12 +3,12 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='account/login.html'), name='login'),
+    path('login/', views.RoleBaseLoginRedict.as_view(template_name='account/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name='account/reset_password.html'), name='reset_password'),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='account/reset_password_sent.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='account/reset_password_confirm.html'), name='password_reset_confirm'),
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='account/reset_password_complete.html'), name='password_reset_complete'),
-    # path('register/', views.register, name='register'),
+    path('register/', views.register, name='register'),
     # path('profile/', views.profile, name='profile'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
 
 
+def register(request):
+    if request.method == 'POST':
+        # Handle registration logic here
+        pass
+    return render(request, 'account/register.html')
+
+class RoleBaseLoginRedict(LoginView):
+    def get_success_url(self):
+
+        user = self.request.user
+
+        if user.role == 'admin':
+            return '/admin/'
+        elif user.role == 'manager':
+            return '/dashboard/'
+        elif user.role == 'stockp_manager':
+            return '/dashboard/'
+        elif user.role == 'sales_manager':
+            return '/dashboard/'
+        elif user.role == 'accountant':
+            return '/dashboard/'
+        elif user.role == 'cashier':
+            return '/pos/'
+        else:
+            return '/account/login/'
