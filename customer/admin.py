7da3fd85c3083ac97eca0accb03a68341f902d10
SupRaKoshti <koshtisahil02@gmail.com>
2025-12-14 21:06:15 +0530

    Add admin configurations for LoyaltyPointsConfig and LoyaltyPointsTransaction models

--- customer/admin.py
+++ customer/admin.py
@@ -6,3 +6,14 @@ from .models import Customer, LoyaltyPointsConfig, LoyaltyPointsTransaction
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ('name', 'phone_number', 'email', 'loyalty_points_achieved')
     search_fields = ('name', 'email', 'phone_number')
+
+@admin.register(LoyaltyPointsConfig)
+class LoyaltyPointsConfigAdmin(admin.ModelAdmin):
+    list_display = ('points_per_rupee', 'point_value_in_rupees', 'maximum_redeem_percentage', 'minimum_purchase_amount', 'minimum_points_to_redeem', 'points_expiration_days', 'is_active')
+    list_filter = ('points_per_rupee', 'point_value_in_rupees', 'maximum_redeem_percentage', 'minimum_purchase_amount', 'minimum_points_to_redeem', 'points_expiration_days', 'is_active')
+
+@admin.register(LoyaltyPointsTransaction)
+class LoyaltyPointsTransactionAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'transaction_type', 'points', 'points_balance_after', 'sale', 'purchase_amount')
+    search_fields = ('sale')
+    list_filter = ('customer', 'transaction_type', 'status')
