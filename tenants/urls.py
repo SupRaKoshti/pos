2fec6687b74ced47c3c74a2be9361f828724cdaa
SupRaKoshti <koshtisahil02@gmail.com>
2026-01-10 21:30:29 +0530

    Add TenantListView for admin access to tenant listings

--- tenants/urls.py
+++ tenants/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import TenantSignUpView, TenantSignInView
+from .views import TenantSignUpView, TenantSignInView, TenantListView
 
 urlpatterns = [
     path('signup/', TenantSignUpView.as_view(), name='tenant-signup'),
     path('signin/', TenantSignInView.as_view(), name='tenant-signin'),
+    path('list/', TenantListView.as_view(), name="tenant-list")
 ]
--- tenants/views.py
+++ tenants/views.py
@@ -3,7 +3,7 @@ from django.views import View
 
 from rest_framework import generics, status
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAdminUser
 from rest_framework.views import APIView
 
 
@@ -72,5 +72,10 @@ class TenantSignInView(APIView):
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
-
-
+class TenantListView(generics.ListAPIView):
+    """
+    Docstring for TenantListView
+    """
+    queryset = Tenant.objects.all()
+    serializer_class = TenantSerializer
+    permission_classes = [IsAdminUser]
